6a18ab24699e6a2b035d02f2dcda4d868988a16b
Hamza khan <srk123boss@gmail.com>
2026-04-20 10:59:27 +0530

    added dependencies for the project

---
+++ src/main/java/com/HireFlow/HireFlowApplication.java
@@ -0,0 +1,13 @@
+package com.HireFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HireFlow/HireFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HireFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
